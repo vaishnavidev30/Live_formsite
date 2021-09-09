172bd146d433f254bdf4a681429c15c84e9ce636
unknown <vaishnavi.devalankar@angularminds.com>
2021-09-09 12:12:53 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import './App.css';
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route, Switch,Link } from "react-router-dom";
+import { BrowserRouter as Router,HashRouter, Route, Switch,Link } from "react-router-dom";
 import Home from './components/Home';
 import FormPage from './components/FormPage';
 
 function App() {
   return (
     
-      <Router>
+      <HashRouter>
          {/* <header >
         <Navbar variant="light" bg="light" >
             <Nav className="justify-content-end">
@@ -26,7 +26,7 @@ function App() {
       <Route exact path="/"component={FormPage} />
       <Route path="/create-form" component={FormPage} />
     </Switch>
-      </Router>
+      </HashRouter>
   
   );
 }
